338f2daadb0d07e5562bb163fa883be12f0510a0
Emil Sievänen <emilsievanen@Emil-MBP.fritz.box>
2021-01-02 20:48:31 +0200

    fixed tests

--- test-client/test-client.js
+++ test-client/test-client.js
@@ -1,46 +1,52 @@
+/*-----------------------------------------------------------
+Module imports*/
+
 import {updateTime, getTime, parseApiResponse} from "../public/js/weather.js";
 const assert = chai.assert;
 import {weatherApiResponse} from "./weather-api-response.js";
 
+/*-----------------------------------------------------------
+Test cases*/
+
 describe("updateTime", () => {
     it("Returns '05'", () => {
-        const input = "5";
-        const expected = "05";
-        const result = updateTime(input);
-        assert.equal(result, "05");
+      const input = "5";
+      const expected = "05";
+      const result = updateTime(input);
+      assert.equal(result, "05");
     });
     it("Returns '10'", () => {
-        const input = "10";
-        const expected = "10";
-        const result = updateTime(input);
-        assert.equal(result, "10");
+      const input = "10";
+      const expected = "10";
+      const result = updateTime(input);
+      assert.equal(result, "10");
     });
 });
 
 describe("getTime", () => {
     it("Returns object", () => {
-        const expected = "object";
-        const result = getTime();
-        assert.equal(typeof result, expected);
+      const expected = "object";
+      const result = getTime();
+      assert.equal(typeof result, expected);
     });
     it("Returns object with a length of 2", () => {
-        const expected = 2;
-        const result = getTime();
-        assert.equal(Object.keys(result).length, expected);
+      const expected = 2;
+      const result = getTime();
+      assert.equal(Object.keys(result).length, expected);
     });
 });
 
 describe("parseApiResponse", () => {
     it("Returns an object", () => {
-            const input = weatherApiResponse;
-            const expected = "object";
-            const result = typeof parseApiResponse(input);
-            assert.equal(result, expected);
+      const input = weatherApiResponse;
+      const expected = "object";
+      const result = typeof parseApiResponse(input);
+      assert.equal(result, expected);
     });
     it("Returns object with a length of 8", () => {
-        const input = weatherApiResponse;
-        const expected = 8;
-        const result = Object.keys(parseApiResponse(input)).length;
-        assert.equal(result, expected);
+      const input = weatherApiResponse;
+      const expected = 8;
+      const result = Object.keys(parseApiResponse(input)).length;
+      assert.equal(result, expected);
     });
 });
--- test/test-server.js
+++ test/test-server.js
@@ -8,15 +8,18 @@ const nock = require("nock");
 const should = chai.should();
 require("dotenv").config();
 
-
 chai.use(chaiHttp);
 
+/*-----------------------------------------------------------
+Test cases*/
+
 describe("Server GET '/'", () => {
-    it("Response 200", () => {
+    it("Response 200", (done) => {
         chai.request(server)
             .get("/")
             .end((err, res) => {
                 res.should.have.status(200);
+                done();
             });
     });
 });
@@ -29,13 +32,14 @@ describe("Server POST '/weather'", () => {
             .get(`/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
             .reply(200, "response");
     });
-    it("Response 200", () => {
+    it("Response 200 OK", (done) => {
         chai.request(server)
             .post("/weather")
             .send("city=Helsinki")
             .end((err, res) => {
                 res.should.have.status(200);
                 res.text.should.equal("response");
+                done();
             });
     });
 });
@@ -48,13 +52,14 @@ describe("Server POST '/map'", () => {
             .get(`/styles/v1/mapbox/streets-v11/static/24.9342,60.1756,10,0,0/434x250?access_token=${apiKey}`)
             .reply(200, "response");
     });
-    it("Response 200", () => {
+    it("Response 200 OK", (done) => {
         chai.request(server)
             .post("/map")
             .send("city=Helsinki")
             .end((err, res) => {
                 res.should.have.status(200);
                 res.text.should.equal("response");
+                done();
             });
     });
 });
